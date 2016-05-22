908c0bffd3597b90376df2e506fd91f401c134a8
Jerome Gravel-Niquet <jeromegn@gmail.com>
2016-05-22 13:01:16 -0400

    fix for crystal 0.16 and up

--- src/bson/db_pointer.cr
+++ src/bson/db_pointer.cr
@@ -1,10 +1,11 @@
 module BSON
   struct DBPointer
     include BSON::Value
-    
-    def initialize(@pointer = "")
+    @ref : BSON::ObjectId
+
+    def initialize(@ref)
     end
-    forward_missing_to @pointer
+    forward_missing_to @ref
 
     def self.from_bson(bson : IO)
       new ObjectId.from_bson(bson)
